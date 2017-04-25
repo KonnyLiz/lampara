7500f12f9d55071a55f0caeb6a71dceea79c1cd5
KonnyAmaya <konyliz_ab@hotmail.com.ar>
2017-04-25 02:42:39 -0600

    lampara controlada por teclado

---
+++ build/CMakeFiles/lamp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lamp.dir/main.cpp.o"
+  "lamp.pdb"
+  "lamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
